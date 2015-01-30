dec830346b83f35945977e592967780212469dda
stephane-ruhlmann <q_nox@ymail.com>
2015-01-30 15:11:08 +0100

    Offline about page can open logs

--- src/systray/aboutdialog.cpp
+++ src/systray/aboutdialog.cpp
@@ -6,15 +6,14 @@ AboutDialog::AboutDialog(QWidget *parent, int width, int height) :
     aboutWebView = new QWebView(this);
     aboutWebView->page()->setLinkDelegationPolicy(QWebPage::DelegateAllLinks);
     this->resize(width, height);
-    this->aboutWebView->resize(this->size());
 }
 
 void AboutDialog::show(){
     aboutWebView->load(QUrl("qrc:/webkit-sources/about.html"));
-    connect(aboutWebView, SIGNAL(linkClicked(QUrl)), this, SLOT(openLink(QUrl)) );
+    connect(aboutWebView, SIGNAL(linkClicked(QUrl)), this, SLOT(openLink(QUrl)));
     this->showNormal();
-    qDebug()<<QString::number(this->size().height())<<QString::number(this->size().width());
-    qDebug()<<QString::number(aboutWebView->size().height())<<QString::number(aboutWebView->size().width());
+    aboutWebView->resize(this->width(), aboutWebView->height());
+    this->adjustSize();
 }
 
 void AboutDialog::openLink(QUrl link){
